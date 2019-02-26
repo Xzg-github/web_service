611db271913f0e6f010c02b8ca9c79604717c04b
liuzhiqiang@hercules-logistics.com <liou5liou20@126.com>
2019-02-26 11:51:30 +0800

    价格管理提交 联系人提交 个人认证修改bug

--- src/api/signature/file_management/contacts/index.js
+++ src/api/signature/file_management/contacts/index.js
@@ -4,16 +4,33 @@ import {host} from '../../../gloablConfig';
 
 let api = express.Router();
 
+const serivce = `${host}/fadada-service-lam`;
+
 // 获取UI标签
 api.get('/config', async (req, res) => {
   const module = await require('./config');
   res.send({returnCode: 0, result: module.default});
 });
 
+
+/*联系人组*/
+
 // 树列表
 api.get('/tree', async (req, res) => {
-  const module = await require('./data');
-  res.send({returnCode: 0, result: module.default});
+  const url = `${serivce}/company_contact_group/tree`;
+  res.send(await fetchJsonByNode(req,url))
+});
+
+//新增组
+api.post('/addGroup',async(req,res) => {
+  const url = `${serivce}/company_contact_group/add`;
+  res.send(await fetchJsonByNode(req,url,postOption(req.body)))
+});
+
+//编辑组
+api.post('/editGroup',async(req,res) => {
+  const url = `${serivce}/company_contact_group/add`;
+  res.send(await fetchJsonByNode(req,url,postOption(req.body)))
 });
 
 // 获取主列表数据
