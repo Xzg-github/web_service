6b93fbf513f74461f9394c8eda11b1ef18238838
肖强 <xiaoqiang@hercules-logistics.com>
2019-03-01 15:40:58 +0800

    企业订单暂存

--- src/routes/signature/businessOrder/EditPage.js
+++ src/routes/signature/businessOrder/EditPage.js
@@ -1,7 +1,8 @@
 import React, {PropTypes} from 'react';
-import {Card, Title, Indent, SuperForm, SuperToolbar, SuperTable} from '../../../components';
+import {Card, Title, Indent, SuperForm, SuperToolbar, SuperTable, SuperPagination} from '../../../components';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './EditPage.less';
+import { getObject } from '../../../common/common';
 
 /*
   企业订单查看消费明细页面
@@ -9,6 +10,10 @@ import s from './EditPage.less';
   consumptionStatisticsInfo: 消费统计列表
   consumptionDetail: 消费明细列表
  */
+
+const PAGINATION = ['maxRecords', 'pageSize', 'currentPage', 'pageSizeType', 'description',
+  'onPageNumberChange', 'onPageSizeChange'];
+
 class EditPage extends React.Component{
   static propTypes = {
     baseInfo: PropTypes.array.isRequired,
@@ -36,12 +41,57 @@ class EditPage extends React.Component{
       checkbox: false,
       index: false,
       cols: consumptionStatisticsInfo,
-      items: value
+      items: value.table1
     };
     return <SuperTable {...tableProps}/>
   };
 
   toConsumptionDetail = () => {
+    const {consumptionDetail, value} = this.props;
+    const table2Props = {
+      checkbox: false,
+      cols: consumptionDetail,
+      items: value.table2
+    };
+    return <SuperTable {...table2Props}/>
+  };
 
+  toButton = () => {
+    const {editPageButton, onClick} = this.props;
+    const barProps = {
+      size: 'large',
+      buttons: editPageButton,
+      onClick: onClick.bind(null, this.props)
+    };
+    return <SuperToolbar {...barProps}/>
+  };
+
+  toPagination = () => {
+    const props = getObject(this.props, PAGINATION);
+    return <SuperPagination {...props}/>;
+  };
+
+  render() {
+    const baseTitle = '基本信息';
+    const consumptionStatisticsTitle = '消费统计';
+    const consumptionDetailTitle = '消费明细';
+    return(
+      <Card>
+        {this.toTitle(baseTitle)}
+        <Indent>{this.toBaseInfo()}</Indent>
+        <div className={s.table1}>
+          {this.toTitle(consumptionStatisticsTitle)}
+          <Indent>{this.toConsumptionStatisticsTable()}</Indent>
+        </div>
+        {this.toTitle(consumptionDetailTitle)}
+        <Indent>{this.toConsumptionDetail()}</Indent>
+        <div className={s.pagination}>{this.toPagination()}</div>
+        <div className={s.footer}>
+          {this.toButton()}
+        </div>
+      </Card>
+    )
   }
 };
+
+export default withStyles(s)(EditPage);
