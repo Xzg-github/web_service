be4c7857ccaecdaad2e79c0259fb5662986633da
wubinghuan <wubinghuan@hercules-logistics.com>
2019-03-30 16:51:54 +0800

    隐藏签署群组

--- src/api/permission/data.js
+++ src/api/permission/data.js
@@ -11,7 +11,7 @@ const data = {
       //enterprise_documents: '企业文件',
       file_management: '档案管理',
       account_management: '账号管理',
-      signature_group: '签署群组',
+      //signature_group: '签署群组',
       contacts:'联系人',
       //template_management: '模板管理',
       //personal_certification: '个人认证',
@@ -29,7 +29,7 @@ const data = {
      //enterprise_documents: '企业文件',
       worker: '企业员工',
       contacts: '联系人',
-      signature_group: '签署群组',
+      //signature_group: '签署群组',
       //template_management: '模板管理',
       //enterprise_certification: '企业认证',
       enterprise_account_management: '企业账号管理'
