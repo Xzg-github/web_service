14c183a2752ce52947f5cb68474509b810152140
XZG <XZG0221@163.C0M>
2019-05-23 14:54:00 +0800

    截至时间过期提示不可签署和发送

--- src/routes/signature/signature_center/EditPageContainer/createEditPageContainer.js
+++ src/routes/signature/signature_center/EditPageContainer/createEditPageContainer.js
@@ -347,6 +347,8 @@ const createEditPageContainer = (action, getSelfState, getParentState) => {
       const {value, controls1, controls2, tableCols, closeFunc} = getSelfState(getState());
       const URL_SAVE = `/api/signature/signature_center/save`;      //保存
       const URL_SUBMIT = '/api/signature/signature_center/sub';  //提交
+      let date = moment().format('YYYY-MM-DD'); //获取当前时间
+      if(value.signExpirationTime <= date){return showError(('签署截至时间已过期，请重新确认！'))}
         if(!validValue(controls1, value)){   //判断from1必填
           dispatch(action.assign({valid: true}));
           return
